7f35887ac28b3f1abf1fe7abbaeb654e7f79fd7d
suzuk12345 <tatakaka.0301.ini@gmail.com>
2023-10-07 18:20:41 +0900

    "が変数を含むよう修正/テキストの修正
    入力される情報を文字列に統一するため

--- main.sh
+++ main.sh
@@ -14,17 +14,17 @@ do
             userName=
             password=
 
-            echo サービス名を入力してください：
+            echo "サービス名を入力してください："
             read serviceName
-            echo "サービス名："$serviceName >> contents.txt
+            echo "サービス名：$serviceName" >> contents.txt
 
-            echo ユーザー名を入力してください：
+            echo "ユーザー名を入力してください："
             read userName
-            echo "ユーザー名："$userName >> contents.txt
+            echo "ユーザー名：$userName" >> contents.txt
 
-            echo パスワードを入力してください：
+            echo "パスワードを入力してください："
             read password
-            echo "パスワード："$password >> contents.txt
+            echo "パスワード：$password" >> contents.txt
 
             echo "パスワードの追加は成功しました。"
             echo "次の選択肢から入力してください(Add Password/Get Password/Exit)：";;
